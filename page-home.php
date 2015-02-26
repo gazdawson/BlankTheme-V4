7e788d651c8f4026a91003814ed0f466e0c45e0b
gazdawson <gareth@merlincycles.com>
2015-02-26 17:25:28 +0000

    added custom homepage and contact page.

---
+++ page-home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Home
+ * @package Krank Theme
+ */
+?>
+<?php
+	// krank_carousel($slide_type, $id, $controls, $indicators, $captions, $trans)
+	if ($krank['home_slides_switch'] == 1) {
+		krank_carousel('home_slides', 'home-carousel', true, true, true, 'slide'); 
+	}
+?>
+<div class="container">
+	<?php while (have_posts()) : the_post(); ?>
+	  <?php get_template_part('templates/page', 'header'); ?>
+	  <?php get_template_part('templates/content', 'page'); ?>
+	<?php endwhile; ?>
+</div>
+
